82483d78ec440c5cebf8ce55639a24af8a41eea3
Katarzyna Rzesikowska <katarzyna.rzesikowska@gmail.com>
2020-07-18 22:55:57 +0200

    add dupport for bootstrap css with javascript and create base template

--- alegrosz/views/main_views.py
+++ alegrosz/views/main_views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__, url_prefix="/")
 
 
 @main_bp.route('/')
 def home():
-    return "it works"
+    return render_template('home.html')
